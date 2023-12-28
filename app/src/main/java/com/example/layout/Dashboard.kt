d00979a6af9381bcb464aa41591572b7538cb090
localSBP <iwan@comet.net.id>
2023-12-28 17:02:26 +0700

    benerindashboard

--- app/src/main/java/com/example/layout/Dashboard.kt
+++ app/src/main/java/com/example/layout/Dashboard.kt
@@ -3,43 +3,179 @@ package com.example.layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.example.layout.ui.theme.LayoutTheme
 
 class Dashboard : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutTheme {
-                dashboard()
+                val showMenu = remember { mutableStateOf(false) }
+
+                TopAppBar(title = {
+                    Text(text = "Dashboard")
+                },
+                    colors = TopAppBarDefaults.topAppBarColors(Color.Blue),
+                    navigationIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                painterResource(id = R.drawable.baseline_menu_24),
+                                contentDescription = null
+                            )
+                        }
+//                        DropdownMenu(
+//                            expanded = showMenu.value,
+//                            onDismissRequest = { showMenu.value = false }) {
+//                            DropdownMenuItem(
+//                                text = {
+//                                    Text(text = "Contoh 1")
+//                                },
+//                                onClick = { /*TODO*/ })
+//                            DropdownMenuItem(
+//                                text = {
+//                                    Text(text = "Contoh 2")
+//                                },
+//                                onClick = { /*TODO*/ })
+//                            DropdownMenuItem(
+//                                text = {
+//                                    Text(text = "Contoh 3")
+//                                },
+//                                onClick = { /*TODO*/ })
+//                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { showMenu.value = true }) {
+                            Icon(
+                                painterResource(id = R.drawable.baseline_more_vert_24),
+                                contentDescription = null
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = showMenu.value,
+                            onDismissRequest = { showMenu.value = false }) {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(text = "Profil")
+                                },
+                                onClick = { /*TODO*/ })
+                            DropdownMenuItem(
+                                text = {
+                                    Text(text = "Paket WiFi")
+                                },
+                                onClick = { /*TODO*/ })
+                            DropdownMenuItem(
+                                text = {
+                                    Text(text = "Ajukan Pemasangan")
+                                },
+                                onClick = { /*TODO*/ })
+                            DropdownMenuItem(
+                                text = {
+                                    Text(text = "Pembayaran")
+                                },
+                                onClick = { /*TODO*/ })
+                        }
+                    }
+
+                )
             }
         }
     }
 }
 
-@androidx.compose.runtime.Composable
-fun dashboard() {
-    Column {
-        Text(text = "Dashboard")
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun dashboard() {
+//    val showMenu = remember { mutableStateOf(false) }
+//
+//    TopAppBar(title = {
+//        Text(text = "Dashboard")
+//    },
+//        colors = TopAppBarDefaults.topAppBarColors(Color.Blue),
+//        navigationIcon = {
+//            IconButton(onClick = { /*TODO*/ }) {
+//                Icon(
+//                    painterResource(id = R.drawable.baseline_menu_24),
+//                    contentDescription = null
+//                )
+//            }
+//            DropdownMenu(
+//                expanded = showMenu.value,
+//                onDismissRequest = { showMenu.value = false }) {
+//                DropdownMenuItem(
+//                    text = {
+//                           Text(text = "Contoh 1")
+//                           },
+//                    onClick = { /*TODO*/ })
+//                DropdownMenuItem(
+//                    text = {
+//                        Text(text = "Contoh 2")
+//                    },
+//                    onClick = { /*TODO*/ })
+//                DropdownMenuItem(
+//                    text = {
+//                        Text(text = "Contoh 3")
+//                    },
+//                    onClick = { /*TODO*/ })
+//            }
+//        },
+//        actions = {
+//            IconButton(onClick = { showMenu.value = true }) {
+//                Icon(
+//                    painterResource(id = R.drawable.baseline_more_vert_24),
+//                    contentDescription = null
+//                )
+//            }
+//            DropdownMenu(
+//                expanded = showMenu.value,
+//                onDismissRequest = { showMenu.value = false }) {
+//                DropdownMenuItem(
+//                    text = {
+//                        Text(text = "Profil")
+//                    },
+//                    onClick = { /*TODO*/ })
+//                DropdownMenuItem(
+//                    text = {
+//                        Text(text = "Paket WiFi")
+//                    },
+//                    onClick = { /*TODO*/ })
+//                DropdownMenuItem(
+//                    text = {
+//                        Text(text = "Ajukan Pemasangan")
+//                    },
+//                    onClick = { /*TODO*/ })
+//                DropdownMenuItem(
+//                    text = {
+//                        Text(text = "Pembayaran")
+//                    },
+//                    onClick = { /*TODO*/ })
+//            }
+//        }
+//
+//        )
+//}
+//
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun dashboardPreview() {
+//    LayoutTheme {
+//        dashboard()
+//    }
+//}
 
-    }
 
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun dashboardPreview() {
-    LayoutTheme {
-        dashboard()
-    }
-}
 //@Composable
 //fun Greeting(name: String, modifier: Modifier = Modifier) {
 //    Text(
--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -1,142 +1,58 @@
 package com.example.layout
 
-import android.content.Intent
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
-import android.icu.text.Transliterator.Position
 import android.os.Bundle
-import android.provider.MediaStore.Images.ImageColumns
-import android.view.textclassifier.TextLinks.TextLink
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Space
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.focusGroup
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.materialIcon
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.modifier.modifierLocalMapOf
-import androidx.compose.ui.modifier.modifierLocalOf
-import androidx.compose.ui.platform.InspectableModifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import androidx.core.content.contentValuesOf
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.example.layout.ui.theme.LayoutTheme
-import org.w3c.dom.Text
-import kotlin.math.max
-import kotlin.math.min
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Column(modifier = Modifier
-                .fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(
-                    painter = painterResource(R.drawable.comet_dark),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .sizeIn(maxWidth = 100.dp, maxHeight = 100.dp)
-                )
-                Text(text = "SELAMAT DATANG",
-                    fontWeight = FontWeight.Bold
-                )
-                Button(onClick = {
-                    val daftar = Intent(this@MainActivity, Daftar::class.java)
-                    startActivity(daftar)
-                },modifier = Modifier
-                    .fillMaxWidth()
-                    ) {
-                    Text(text ="Daftar",
-                        color = Color.White)
-                }
-                Button(onClick = {
-                    val masuk = Intent(this@MainActivity,Masuk::class.java)
-                    startActivity(masuk)
-                },
-                    modifier = Modifier
-                    .fillMaxWidth(),) {
-                    Text(text = "MASUK",
-                        color = Color.White)
-                }
-            }
-//            Screen()
-//            daftar()
-//            Masuk()
-//            Greeting("Android")
+            Dashboard()
+//            Column(modifier = Modifier
+//                .fillMaxWidth(),
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally) {
+//                Image(
+//                    painter = painterResource(R.drawable.comet_dark),
+//                    contentDescription = null,
+//                    modifier = Modifier
+//                        .align(Alignment.CenterHorizontally)
+//                        .sizeIn(maxWidth = 100.dp, maxHeight = 100.dp)
+//                )
+//                Text(text = "SELAMAT DATANG",
+//                    fontWeight = FontWeight.Bold
+//                )
+//                Button(onClick = {
+//                    val daftar = Intent(this@MainActivity, Daftar::class.java)
+//                    startActivity(daftar)
+//                },modifier = Modifier
+//                    .fillMaxWidth()
+//                    ) {
+//                    Text(text ="Daftar",
+//                        color = Color.White)
+//                }
+//                Button(onClick = {
+//                    val masuk = Intent(this@MainActivity,Masuk::class.java)
+//                    startActivity(masuk)
+//                },
+//                    modifier = Modifier
+//                    .fillMaxWidth(),) {
+//                    Text(text = "MASUK",
+//                        color = Color.White)
+//                }
+//            }
         }
     }
 }
 
+
+
+
+//            Screen()
+//            daftar()
+//            Masuk()
+//            Greeting("Android")
 //@Composable
 //fun UserChat(name:String,msg:String) {
 //    Row(modifier = Modifier.padding(4.dp)) {
